07f4d0cf7403d9d7890a92e6e313171d67c3c928
satnam <satnamkahlon4@gmail.com>
2021-05-14 20:19:33 +0530

    after adding polyline and render it

--- Map_App_demo1/ViewController.swift
+++ Map_App_demo1/ViewController.swift
@@ -11,10 +11,19 @@ import MapKit
 class ViewController: UIViewController ,CLLocationManagerDelegate{
 
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var locationbtn: UIButton!
+    
     var locationManager = CLLocationManager()
+    var destination : CLLocationCoordinate2D!
+    
+    var transportType : MKDirectionsTransportType!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //adding map delegate to show polyline on map
+        map.delegate = self
         
         //assigning location delegate
         locationManager.delegate = self
@@ -27,6 +36,8 @@ class ViewController: UIViewController ,CLLocationManagerDelegate{
         
         //start location update
         locationManager.startUpdatingLocation()
+        
+        locationbtn.isHidden = true
         
         // following is used for long press
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPress))
@@ -90,17 +101,73 @@ class ViewController: UIViewController ,CLLocationManagerDelegate{
         
         //to remove all the previous annotations
         map.removeAnnotations(map.annotations)
+        map.removeOverlays(map.overlays)
         
         let location = gestureRecognizer.location(in: map)
         let coordinates = map.convert(location, toCoordinateFrom: map)
         
         //adding annotation
         let annotation = MKPointAnnotation()
-        annotation.title = "My places"
+        annotation.title = "Destination"
         annotation.coordinate = coordinates
         map.addAnnotation(annotation)
+        
+        locationbtn.isHidden = false
+        destination = coordinates
+        
     }
     
+    @IBAction func drawRoute(_ sender: UIButton) {
+        //this will remove all the overlays
+        map.removeOverlays(map.overlays)
+        
+        //getting the destination
+        let sourcePlaceMarker = MKPlacemark(coordinate: locationManager.location!.coordinate)
+        let destinationPlaceMarker = MKPlacemark(coordinate: destination)
+        
+        //request direction
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = MKMapItem(placemark: sourcePlaceMarker)
+        directionRequest.destination = MKMapItem(placemark: destinationPlaceMarker)
+        
+        //transport type
+        directionRequest.transportType = .automobile
+        
+        //calculating the direction
+        let directions =  MKDirections(request: directionRequest)
+        directions.calculate{ response,error in
+            guard let directionResponse = response else {return}
+            
+            //create route
+            let route = directionResponse.routes[0]
+            //draw polyline
+            self.map.addOverlay(route.polyline, level: .aboveRoads)
+            
+            //define map boundary for screen
+            let rect = route.polyline.boundingMapRect
+            self.map.setVisibleMapRect(rect, edgePadding: UIEdgeInsets(top:100,left:100,bottom:100,right:100), animated: true)
+            
+            
+        }
+    }
+}
 
+extension ViewController : MKMapViewDelegate{
+    //for rendering the polyline
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle{
+            let renderer = MKCircleRenderer(overlay: overlay)
+            renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            renderer.strokeColor = .green
+            renderer.lineWidth = 2
+            return renderer
+        }else if overlay is MKPolyline{
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .blue
+            renderer.lineWidth = 5
+            return renderer
+        }
+        return MKOverlayRenderer()
+    }
 }
 
